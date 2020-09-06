64dfe39deb6ff6029ba378f3fa708c4c13b00c36
LudovicSoltys <moa_ludo@yahoo.fr>
2020-09-06 21:38:29 +0200

    fixing some config

--- src/main/java/com/example/converter/shared/BadRequestExceptionHandler.java
+++ src/main/java/com/example/converter/shared/BadRequestExceptionHandler.java
@@ -6,14 +6,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * an exception handler for bad requests
  */
-@ControllerAdvice
-class BadRequestExceptionHandler {
+@EnableWebMvc
+@ControllerAdvice(basePackages = {"com.example.converter.v1", "com.example.converter.v2", "com.example.converter.v3"})
+public class BadRequestExceptionHandler {
 
-    @ExceptionHandler(value = { NumberFormatException.class, IllegalArgumentException.class })
+    @ExceptionHandler(IllegalArgumentException.class)
     protected ResponseEntity<Object> handleBadRequest(RuntimeException ex, WebRequest request) {
 
         return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
